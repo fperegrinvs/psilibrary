f2c9cd7600b45c31b6519aff3eb76bcb6ad44ad7
Leonardo Stern <leonardo.stern@gmail.com>
2015-08-11 22:21:25 -0300

    #35 crud do entrytype

--- server/repositories/entryTypeRepository.go
+++ server/repositories/entryTypeRepository.go
@@ -7,6 +7,43 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+func AddEntryType(e *models.EntryType) {
+	db, err := sql.Open(conf.Db, conf.Conn)	
+	defer db.Close()
+
+	_, err = db.Exec("insert into EntryType (Name) values (?)", e.Name)
+
+	if err != nil {}
+}
+
+func DeleteEntryType(id int) {
+	db, err := sql.Open(conf.Db, conf.Conn)	
+	defer db.Close()
+
+	_, err = db.Exec("delete from EntryType where IdEntryType = ?", id)
+
+	if err != nil {}
+}
+
+
+func GetEntryTypeById(id int) *models.EntryType {
+	db, err := sql.Open(conf.Db, conf.Conn)	
+	defer db.Close()
+
+	rows, err := db.Query("SELECT EntryTypeId, Name FROM entrytype where EntryTypeId = ?", id)
+
+	for rows.Next() {
+	    e := new(models.EntryType)
+	    if err := rows.Scan(&e.ID, &e.Name); err != nil { }
+	    return e
+	}
+
+	if err != nil {
+	}
+
+	return nil
+}
+
 func ListEntryTypes() []*models.EntryType {
 	var entries []*models.EntryType
 
