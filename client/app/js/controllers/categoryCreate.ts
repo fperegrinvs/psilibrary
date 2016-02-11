aeec37a3396333e848a2808efdcbee6023980ac6
Leonardo Stern <leonardo.stern@gmail.com>
2016-02-11 20:53:15 -0200

    #42 - copiando telas do tipo de registro para categoria

--- client/app/js/controllers/categoryCreate.ts
+++ client/app/js/controllers/categoryCreate.ts
@@ -1,4 +1,15 @@
 'use strict'; // http://stackoverflow.com/questions/1335851/what-does-use-strict-do-in-javascript-and-what-is-the-reasoning-behind-it
+
 angular.module('psilibrary.controllers')
-    .controller('categoryCreateCtl', ['$scope', function ($scope) {
+    .controller('categoryCreateCtl', ['$scope', 'categoryService', '$state', function($scope, categoryService, $state) {
+		window.currentScope = $scope;
+
+		$scope.data = {};
+
+		$scope.update = function() {
+			categoryService.Create($scope.data).then(function() {
+				$state.go('category')
+			});
+		}
+
     }]);
