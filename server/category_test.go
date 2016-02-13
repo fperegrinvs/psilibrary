0c05686831a3625179fd1d45c0e96d8e85787350
Leonardo Stern <leonardo.stern@gmail.com>
2016-02-13 09:51:13 -0200

    #54 - start using ginkgo

---
+++ server/category_test.go
@@ -0,0 +1,49 @@
+package main
+import  "testing"
+
+
+func CreatingNewCategory(t *testing.T) {
+	t.Error("Need to implement Test")
+}
+
+func CheckCategory(t *testing.T){
+	t.Error("Need to implement Test")
+}
+
+func CheckCategoryInvalidParent(t *testing.T){
+	t.Error("Need to implement Test")
+}
+
+func ListingAllCategories(t *testing.T) {
+	t.Error("Need to implement Test")
+}
+
+func GettingACategory(t *testing.T) {
+	t.Error("Need to implement Test")
+}
+
+func GetInvalidCategory(t *testing.T){
+	t.Error("Need to implement Test")
+}
+
+
+func UpdatingACategory(t *testing.T) {
+	t.Error("Need to implement Test")
+}
+
+func DeletingACategory(t *testing.T) {
+	t.Error("Need to implement Test")
+}
+
+func CheckIfCategoryIsUsed(t *testing.T) {
+	t.Error("Need to implement Test")
+}
+
+func CheckIfCategoryIsUsedInvalid(t *testing.T) {
+	t.Error("Need to implement Test")
+}
+
+
+func CheckCategoryMethodsRoutes(t *testing.T){
+	t.Error("Need to implement Test")
+}
--- server/server_suite_test.go
+++ server/server_suite_test.go
@@ -8,6 +8,10 @@ import (
 )
 
 func TestServer(t *testing.T) {
+    if testing.Short() {
+      t.Skip("skipping test in short mode.")
+      return
+    }	
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Server Suite")
 }
