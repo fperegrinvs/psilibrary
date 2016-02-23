a5c33c421ecafbc2ff4335468498766ff7fd61b7
Leonardo Stern <leonardo.stern@gmail.com>
2016-02-23 11:25:58 -0300

    using sqlx

--- server/repositories/repository.go
+++ server/repositories/repository.go
@@ -2,17 +2,22 @@ package repositories
 import (
 	"database/sql"
 	"github.com/lstern/psilibrary/server/conf"
+	"github.com/jmoiron/sqlx"
 )
 
 type Repository struct{
 	DB *sql.DB
 }
 
-func openSql(db *sql.DB) (*sql.DB, error){
+func openSql(db *sql.DB) (*sqlx.DB, error){
 	if (db != nil){
-		return db, nil
+		return sqlx.NewDb(db, conf.Db), nil
 	}
-	
+
 	db, err := sql.Open(conf.Db, conf.Conn)
-	return db, err	
+	if (err != nil){ 
+		return nil, err
+	}
+
+	return sqlx.NewDb(db, conf.Db), err	
 }
