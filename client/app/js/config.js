cd7dfa71524853e87ca80f45e556491c883bc838
Leonardo Stern <leonardo.stern@gmail.com>
2016-05-03 18:21:35 -0300

    fix table width

--- client/app/js/config.js
+++ client/app/js/config.js
@@ -2,7 +2,7 @@ var Psilibrary;
 (function (Psilibrary) {
     angular.module('psilibrary.config')
         .constant('config', {
-        'serverUrl': 'http://localhost:8080',
+        'serverUrl': 'http://52.67.51.36',
         'retries': 3
     });
 })(Psilibrary || (Psilibrary = {}));
