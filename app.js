76d6326c043367fd5dd4d390cd1c6081b700a922
Leonardo Stern <leonardo.stern@gmail.com>
2016-05-05 23:37:40 -0300

    atualização do site

--- app.js
+++ app.js
@@ -83158,6 +83158,7 @@ function defaultPromise(h, $q){
     angular.module('psilibrary.config')
         .constant('config', {
         'serverUrl': 'http://52.67.51.36',
+//        'serverUrl': 'http://localhost:8080',
         'retries': 3
     });
 })(Psilibrary || (Psilibrary = {}));
